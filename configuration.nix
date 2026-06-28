02c3ebc09883627e8e0ac25fed2a4cadbabb668a
George Hulme <george@crown-shy.com>
2026-06-28 16:28:17 +0100

    Added bash aliases

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,10 @@
   # System packages
   environment.systemPackages = [ ];
 
+  environment.shellAliases = {
+    rebuild-system = "sudo nixos-rebuild switch --flake /etc/nixos";
+    update-system = "sudo nix flake update /etc/nixos && sudo nixos-rebuild switch --flake /etc/nixos";
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

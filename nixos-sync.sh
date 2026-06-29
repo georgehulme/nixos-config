cb2771603fa55f0c0d8f04bfd69cd0d91a59cf7b
George Hulme <george@crown-shy.com>
2026-06-29 12:47:13 +0100

    Add nix-profile to sync rules

--- nixos-sync.sh
+++ nixos-sync.sh
@@ -5,6 +5,7 @@ declare -A CONFIG_FILES=(
     ["hardware-configuration.nix"]="/etc/nixos/hardware-configuration.nix"
     ["flake.nix"]="/etc/nixos/flake.nix"
     ["flake.lock"]="/etc/nixos/flake.lock"
+    ["nix-profile"]="/home/george/.nix-profile"
 )
 
 save_configs() {

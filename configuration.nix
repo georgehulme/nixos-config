442bd1301682dea10dc5a8ab19259a1072823440
George Hulme <george@crown-shy.com>
2026-06-30 09:16:01 +0100

    NixOS Backup: Tue 30 Jun 09:16:01 BST 2026

--- configuration.nix
+++ configuration.nix
@@ -44,7 +44,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
 
   # Configure keymap in X11
